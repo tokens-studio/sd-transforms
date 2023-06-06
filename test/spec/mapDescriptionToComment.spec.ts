f6d4e0dcb71ed391210264cf90ff48035beb99c0
Roger Tuan <roger.tuan@generac.com>
2023-06-06 14:15:19 -0700

    Add new test and fix CONTRIBUTING doc

--- test/spec/mapDescriptionToComment.spec.ts
+++ test/spec/mapDescriptionToComment.spec.ts
@@ -15,5 +15,21 @@ describe('map description to comment', () => {
       description: 'Some description about the token',
       comment: 'Some description about the token',
     });
+
+    expect(
+      mapDescriptionToComment({
+        type: 'dimension',
+        value: '10px',
+        description:
+          'This is the first line.\nThis is the second line.\rThis is the third line.\r\nThis is the fourth line.',
+      }),
+    ).to.eql({
+      type: 'dimension',
+      value: '10px',
+      description:
+        'This is the first line.\nThis is the second line.\rThis is the third line.\r\nThis is the fourth line.',
+      comment:
+        'This is the first line. This is the second line. This is the third line. This is the fourth line.',
+    });
   });
 });
