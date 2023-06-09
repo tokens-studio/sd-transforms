d8eababe704439a3ca97dfc24a18c4ad46a2f181
Max Karkowski <maxkarkowski@gmail.com>
2023-06-10 00:04:30 +0200

    feat: handle comma seperated fontfamily sets (#149)
    * feat: handle comma seperated fontfamily sets
    
    * feat: remove ts-ignore
    
    * feat: create array if its comma seperated string
    
    * feat: add unit test for utils
    
    * chore: add changeset entry
    
    ---------
    
    Co-authored-by: jorenbroekema <joren.broekema@gmail.com>

--- src/css/transformTypography.ts
+++ src/css/transformTypography.ts
@@ -2,7 +2,33 @@ import { transformDimension } from '../transformDimension.js';
 import { transformFontWeights } from '../transformFontWeights.js';
 import { checkAndEvaluateMath } from '../checkAndEvaluateMath.js';
 import { isNothing } from '../utils/is-nothing.js';
-import { hasWhiteSpace } from '../utils/has-whitespace.js';
+
+export function hasWhiteSpace(value: string): boolean {
+  const whiteSpaceRegex = new RegExp('\\s+');
+  return whiteSpaceRegex.test(value);
+}
+
+export function isCommaSeparated(value: string): boolean {
+  return value.includes(',');
+}
+
+function quoteWrapWhitespacedFont(fontString: string) {
+  return hasWhiteSpace(fontString) ? `'${fontString}'` : fontString;
+}
+
+function processFontFamily(fontFamily: string | undefined) {
+  if (isNothing(fontFamily)) {
+    return 'sans-serif';
+  }
+
+  if (isCommaSeparated(fontFamily as string)) {
+    let fontFamilyArray = [];
+    fontFamilyArray = (fontFamily as string).split(',').map(part => part.trim());
+    return fontFamilyArray.map((part: string) => quoteWrapWhitespacedFont(part)).join(', ');
+  }
+
+  return quoteWrapWhitespacedFont(fontFamily as string);
+}
 
 /**
  * Helper: Transforms typography object to typography shorthand for CSS
@@ -18,13 +44,12 @@ export function transformTypographyForCSS(
   }
 
   let { fontFamily, fontWeight, fontSize, lineHeight } = value;
-
-  fontWeight = transformFontWeights(fontWeight);
   fontSize = transformDimension(checkAndEvaluateMath(fontSize));
   lineHeight = checkAndEvaluateMath(lineHeight);
-  fontFamily = hasWhiteSpace(fontFamily as string | undefined) ? `'${fontFamily}'` : fontFamily;
+  fontWeight = transformFontWeights(fontWeight);
+  fontFamily = processFontFamily(fontFamily as string | undefined);
 
   return `${isNothing(fontWeight) ? 400 : fontWeight} ${isNothing(fontSize) ? '16px' : fontSize}/${
     isNothing(lineHeight) ? 1 : lineHeight
-  } ${isNothing(fontFamily) ? 'sans-serif' : fontFamily}`;
+  } ${fontFamily}`;
 }
--- src/utils/has-whitespace.ts
+++
@@ -1,8 +0,0 @@
-export function hasWhiteSpace(value: string | undefined): boolean {
-  const reWhiteSpace = new RegExp('\\s+');
-
-  if (value !== undefined && reWhiteSpace.test(value)) {
-    return true;
-  }
-  return false;
-}
