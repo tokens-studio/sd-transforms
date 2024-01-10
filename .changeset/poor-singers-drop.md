7409419206a1ffc85306fa8fcdade0248a3429d9
Joren Broekema <joren.broekema@gmail.com>
2024-01-10 22:11:09 +0100

    fix: use expr-eval fork to fix security issue (#244)

---
+++ .changeset/poor-singers-drop.md
@@ -0,0 +1,5 @@
+---
+'@tokens-studio/sd-transforms': patch
+---
+
+Use expr-eval-fork to work around a prototype pollution vulnerability in the original package.
--- src/checkAndEvaluateMath.ts
+++ src/checkAndEvaluateMath.ts
@@ -1,4 +1,4 @@
-import { Parser } from 'expr-eval';
+import { Parser } from 'expr-eval-fork';
 import { parse, reduceExpression } from 'postcss-calc-ast-parser';
 
 const mathChars = ['+', '-', '*', '/'];
