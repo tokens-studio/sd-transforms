e3abab0184e73878f99dff012d604fa9e1226fab
Dario Soller <dario.soller@allianz.de>
2025-03-26 17:55:49 +0100

    tests(integration): add one more mix color integration tests

--- test/integration/color-modifier-references.test.ts
+++ test/integration/color-modifier-references.test.ts
@@ -86,10 +86,17 @@ describe('color modifier references', () => {
     expect(content).toBe(expectedOutput);
   });
 
-  it('supports color with referenced base color, referenced mix color, and expression-based mix value', async () => {
+  it('supports color with referenced base color, referenced mix color and reference mix value', async () => {
     const file = await promises.readFile(outputFilePath, 'utf-8');
-    const content = excerpt(file, { start: new RegExp('--sdColor6: .*;'), end: '}' });
+    const content = excerpt(file, { start: new RegExp('--sdColor5: .*;'), end: '--sdColor8' });
     const expectedOutput = `--sdColor7: #3b64b3;`;
     expect(content).toBe(expectedOutput);
+  });
+
+  it('supports color with referenced base color, referenced mix color, and expression-based mix value with references', async () => {
+    const file = await promises.readFile(outputFilePath, 'utf-8');
+    const content = excerpt(file, { start: new RegExp('--sdColor6: .*;'), end: '}' });
+    const expectedOutput = `--sdColor8: #3b64b3;`;
+    expect(content).to.equal(expectedOutput);
   });
 });
